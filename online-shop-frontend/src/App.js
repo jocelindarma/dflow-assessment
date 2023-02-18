a071a969ae62afbfbd1afacd19d28f6aaa2fce83
Jocelin Darma <jocelindarma13@gmail.com>
2023-02-18 15:59:39 -0800

    add checkout page

--- online-shop-frontend/src/App.js
+++ online-shop-frontend/src/App.js
@@ -4,18 +4,23 @@ import { Navbar } from "./components/Navbar";
 import { Shop } from "./pages/Shop";
 import { Cart } from "./pages/Cart";
 import { User } from "./pages/User";
+import { Checkout } from "./pages/Checkout";
+import { ShopContextProvider } from "./context/ShopContext";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />}/>
-          <Route path="/cart" element={<Cart />}/>
-          <Route path="/user" element={<User />}/>
-        </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />}/>
+            <Route path="/cart" element={<Cart />}/>
+            <Route path="/user" element={<User />}/>
+            <Route path="/checkout" element={<Checkout />}/>
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
---
+++ online-shop-frontend/src/pages/Checkout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Checkout = () => {
+  return (
+    <h1> Thank you for your order! </h1>
+  )
+}
