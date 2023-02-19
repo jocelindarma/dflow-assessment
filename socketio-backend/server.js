81400d66c5dd747d19101e33bb3e979bc55a1046
Jocelin Darma <jocelindarma13@gmail.com>
2023-02-19 10:55:13 -0800

    add initial react files

--- socketio-backend/server.js
+++ socketio-backend/server.js
@@ -2,9 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const http = require("http");
+const { Server } = require("socket.io");
 
 app.use(cors());
+
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connect", (socket) => {
+  console.log(`User ${socket.id} is connected`);
+
+  socket.on("join_group", (data) => {
+    socket.join(data);
+    console.log(`User ${socket.id} joined group ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.group).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} is disconnected`);
+  });
+});
 
 server.listen(3001, () => {
   console.log("Server running on port 3001");
