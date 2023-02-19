7271a09ea1dc67f6736e9ec9a6c0dd7649ee43e1
Jocelin Darma <jocelindarma13@gmail.com>
2023-02-18 16:00:00 -0800

    add cart and cart items

--- online-shop-frontend/src/pages/Cart.js
+++ online-shop-frontend/src/pages/Cart.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { PRODUCTS } from "../sampledata";
+import { CartItem } from "./CartItem";
+import { useNavigate } from "react-router-dom";
+import "./Cart.css"
 
 export const Cart = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: Rp {totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <div className="checkout">
+          <h1> Your Shopping Cart is Empty</h1>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+        </div>
+      )}
+    </div>
+  );
+};
--- online-shop-frontend/src/pages/Product.js
+++ online-shop-frontend/src/pages/Product.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from '../context/ShopContext';
 
 export const Product = (props) => {
-  const { id, name, price } = props.data;
+  const { id, name, price, imageUrl} = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
   return (
     <div className="product">
+      <img src={imageUrl} alt="No Image" />
       <div className="description">
         <p>
           <b>{name}</b>
         </p>
         <p> Rp {price}</p>
       </div>
-      <button className="addToCartBtn">Add to Cart</button>
-    </div>
+      <button className="addToCartBtn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+      </button>    </div>
   )
 }
