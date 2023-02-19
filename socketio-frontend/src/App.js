21ab0d30c3d52ff385b4fc2eccd2db2d3f472214
Jocelin Darma <jocelindarma13@gmail.com>
2023-02-19 13:42:02 -0800

    add frontend code

--- socketio-frontend/src/App.js
+++ socketio-frontend/src/App.js
@@ -1,8 +1,46 @@
 import './App.css';
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [name, setName] = useState("");
+  const [group, setGroup] = useState("");
+  const [chatroom, setChatroom] = useState(false);
+
+  const joinGroup = () => {
+    if (name !== "" && group !== "") {
+      socket.emit("join_group", group);
+      setChatroom(true);
+    }
+  };
+
   return (
     <div className="App">
+      {!chatroom ? (
+        <div className="join-container">
+          <h1>DFLOW Indonesia SocketIO Chatroom</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Group ID"
+            onChange={(event) => {
+              setGroup(event.target.value);
+            }}
+          />
+          <button onClick={joinGroup}>Join Group</button>
+        </div>
+      ) : (
+        <Chat socket={socket} name={name} group={group} />
+      )}
     </div>
   );
 }
---
+++ socketio-frontend/src/Chat.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+const Chat = ({ socket, name, group }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const data = {
+        name: name,
+        group: group,
+        message: currentMessage,
+      };
+
+      await socket.emit("send_message", data);
+      setMessages((list) => [...list, data]);
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessages((list) => [...list, data]);
+    });
+    return () => socket.removeListener("receive_message");
+  }, [socket]);
+
+  return (
+    <div className="chat">
+      <div className="chat-header">
+        <p>DFLOW SocketIO Chatroom</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="scroll-container">
+          {messages.map((message) => {
+            return (
+              <div
+                className="message"
+                id={name === message.name ? "you" : ""}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{message.message}</p>
+                  </div>
+                  <div className="message-name">
+                    <p id="name">{message.name}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Type something here..."
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
+          }}
+        />
+        <button onClick={sendMessage}>&#10095;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
